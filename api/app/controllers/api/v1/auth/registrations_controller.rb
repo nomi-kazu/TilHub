02b9e33855b11cf930c33eef6e1a916315025d61
nomi-kazu <nomikazyu@gmail.com>
2021-10-24 23:15:07 +0900

    feat: Deviseに必要な設定を追加

---
+++ api/app/controllers/api/v1/auth/registrations_controller.rb
@@ -0,0 +1,19 @@
+module Api
+  module V1
+    module Auth
+      class RegistrationsController < DeviseTokenAuth::RegistrationsController
+
+        private
+
+        # ストロングパラメーター設定
+        def sign_up_params
+          params.permit(:name, :email, :password, :password_confirmation)
+        end
+
+        def account_update_params
+          params.permit(:name, :email, :nickname, :image)
+        end
+      end
+    end
+  end
+end
--- api/config/routes.rb
+++ api/config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   namespace :api do
     namespace :v1 do
-      # api test action
-      resources :hello, only:[:index]
+      mount_devise_token_auth_for 'User', at: 'auth', controllers: {
+        registrations: 'api/v1/auth/registrations'
+      }
     end
   end
+  mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
 end
