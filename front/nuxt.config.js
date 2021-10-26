42cada12f3d5a227f733df9c68ea0508b6e74c60
nomi-kazu <nomikazyu@gmail.com>
2021-10-27 00:06:20 +0900

    fix: nuxtClientInitの記述修正

--- front/nuxt.config.js
+++ front/nuxt.config.js
@@ -31,8 +31,8 @@ export default {
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
-    '~/plugins/axios',
-    '~/plugins/nuxt-client-init.client.js'
+    '~/plugins/nuxt-client-init.js',
+    '~/plugins/axios'
   ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
