a2d6fd17d0f529098c4c3dd10780b60dd7af7a38
nomi-kazu <nomikazyu@gmail.com>
2021-10-29 18:50:55 +0900

    feat: getterの追加

--- front/store/index.js
+++ front/store/index.js
@@ -1,34 +1,41 @@
 const cookieparser = process.server ? require('cookieparser') : undefined
 
-export const state = () => {
-  return {
-    accessToken: '',
-    uid: '',
-    client: '',
-    id: '',
-    isAuthenticated: false
-  }
+export const state = () => ({
+  accessToken: null,
+  client: null,
+  uid: null,
+  id: null,
+  isAuthenticated: false
+})
+
+export const getters = {
+  accessToken: state => state.accessToken,
+  client: state => state.client,
+  uid: state => state.uid,
+  id: state => state.id,
+  isAuthenticated: state => state.isAuthenticated
 }
+
 export const mutations = {
+  clearUser (state) {
+    state.accessToken = null
+    state.client = null
+    state.uid = null
+    state.id = null
+    state.isAuthenticated = false
+  },
   setUser (state, res) {
     state.accessToken = res.headers['access-token']
-    state.uid = res.headers.uid
     state.client = res.headers.client
+    state.uid = res.headers.uid
     state.id = res.data.data.id
     state.isAuthenticated = true
   },
   setHeader (state, { header, authFlag }) {
     state.accessToken = header['access-token']
-    state.uid = header.uid
     state.client = header.client
+    state.uid = header.uid
     state.isAuthenticated = authFlag
-  },
-  clearUser (state) {
-    state.accessToken = null
-    state.isAuthenticated = false
-    state.uid = null
-    state.client = null
-    state.id = null
   }
 }
 
