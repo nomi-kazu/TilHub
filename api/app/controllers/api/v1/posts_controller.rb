a2d766e8c7dcd0b26437cf2118f981db20bde330
nomi-kazu <nomikazyu@gmail.com>
2021-10-30 17:32:25 +0900

    feat: postテーブルを作成、post_serializerを追加

---
+++ api/app/controllers/api/v1/posts_controller.rb
@@ -0,0 +1,25 @@
+module Api
+  module V1
+    class PostsController < ApplicationController
+
+      def index
+      end
+    
+      def new
+      end
+    
+      def create
+      end
+    
+      def edit
+      end
+    
+      def update
+      end
+    
+      def destroy
+      end
+
+    end
+  end
+end
