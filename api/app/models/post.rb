a1c58848a77a583e056c8b9433ec96d413a1323f
nomi-kazu <nomikazyu@gmail.com>
2021-10-30 17:52:17 +0900

    feat: 各項目をバリデーションの設定

--- api/app/models/post.rb
+++ api/app/models/post.rb
@@ -1,3 +1,7 @@
 class Post < ApplicationRecord
+  validates :name, presence: true, length: { maximum: 100 }
+  validates :content, presence: true
+  validates :public, inclusion: { in: [true, false] }
+
   belongs_to :user
 end
