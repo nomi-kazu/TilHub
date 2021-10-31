4f98b5d22bee1c96730455b1e8b12616cb0ac27d
nomi-kazu <nomikazyu@gmail.com>
2021-10-31 12:03:33 +0900

    feat: templateにcontainerを追加

---
+++ front/components/templates/SigninEnterusernameTemplate.vue
@@ -0,0 +1,10 @@
+<template>
+  <OneColumnContainer>
+  </OneColumnContainer>
+</template>
+
+<script>
+export default {
+
+}
+</script>
---
+++ front/components/templates/UsernameIndexTemplate.vue
@@ -0,0 +1,15 @@
+<template>
+  <TwoColumnContainer>
+    <template #left>
+    </template>
+
+    <template #right>
+    </template>
+  </TwoColumnContainer>
+</template>
+
+<script>
+export default {
+
+}
+</script>
