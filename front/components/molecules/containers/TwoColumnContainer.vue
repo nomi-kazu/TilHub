bf4a96b53f2c1be4c3a61af76819011607597975
nomi-kazu <nomikazyu@gmail.com>
2021-10-31 11:24:47 +0900

    feat: containerを追加

---
+++ front/components/molecules/containers/TwoColumnContainer.vue
@@ -0,0 +1,29 @@
+<template>
+  <BaseContainer>
+    <v-row justify="center">
+      <v-col :cols="leftCols">
+        <slot name="left" />
+      </v-col>
+
+      <v-col :cols="rightCols">
+        <slot name="right" />
+      </v-col>
+    </v-row>
+  </BaseContainer>
+</template>
+
+<script>
+export default {
+  props: {
+    leftCols: {
+      type: Number,
+      default: 8
+    },
+
+    rightCols: {
+      type: Number,
+      default: 4
+    }
+  }
+}
+</script>
