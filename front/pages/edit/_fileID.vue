e739292bcfb53ee34781d26ea84e1e86ac2bba7d
nomi-kazu <nomikazyu@gmail.com>
2021-10-31 11:00:38 +0900

    feat: edit/_fileIDの処理を追加

--- front/pages/edit/_fileID.vue
+++ front/pages/edit/_fileID.vue
@@ -1,10 +1,18 @@
 <template>
   <div>
     <p>ファイル名：{{ fileID }}</p>
+
+    <EditFileidSBothTemplate v-if="isBoth" />
+
+    <EditFileidSEditTemplate v-if="isEdit" />
+
+    <EditFileidSViewTemplate v-if="isView" />
   </div>
 </template>
 
 <script>
+const DEFALUT_STATUS = 'both'
+
 export default {
   middleware: 'authenticated',
 
@@ -12,8 +20,27 @@ export default {
     fileID () {
       return this.$route.params.fileID
     },
-    status () {
-      return this.$route.query.status
+
+    defaultStatus () {
+      return DEFALUT_STATUS
+    },
+
+    isBoth () {
+      const LABEL = 'both'
+      const status = this.$route.query.status
+      return status ? status === LABEL : this.defaultStatus === LABEL
+    },
+
+    isEdit () {
+      const LABEL = 'edit'
+      const status = this.$route.query.status
+      return status ? status === LABEL : this.defalutStatus === LABEL
+    },
+
+    isView () {
+      const LABEL = 'view'
+      const status = this.$route.query.status
+      return status ? status === LABEL : this.defalutStatus === LABEL
     }
   }
 }
