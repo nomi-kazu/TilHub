fb609f9a3539b5ccc7c18f41485b21095b172d99
nomi-kazu <nomikazyu@gmail.com>
2021-10-31 14:28:58 +0900

    feat: signupの追加

---
+++ front/components/organisms/cards/SignupCard.vue
@@ -0,0 +1,11 @@
+<template>
+  <v-card>
+    <SignupForm />
+  </v-card>
+</template>
+
+<script>
+export default {
+
+}
+</script>
--- front/components/templates/SignupTemplate.vue
+++ front/components/templates/SignupTemplate.vue
@@ -1,5 +1,8 @@
 <template>
   <OneColumnContainer>
+    <h1>新規登録</h1>
+
+    <SigninCard />
   </OneColumnContainer>
 </template>
 
