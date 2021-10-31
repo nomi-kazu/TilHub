99118eeb35028d534e1ddaab7d1edc867d58dcc3
nomi-kazu <nomikazyu@gmail.com>
2021-10-31 18:34:33 +0900

    feat: eslintの設定を追加

--- front/.eslintrc.js
+++ front/.eslintrc.js
@@ -10,19 +10,20 @@ module.exports = {
   },
   extends: [
     '@nuxtjs',
-    'plugin:nuxt/recommended'
+    'plugin:nuxt/recommended',
+    'prettier'
   ],
   plugins: [
   ],
   // add your custom rules here
   rules: {
-    // 'semi': [2, 'never'],
-    // 'no-console': 'off',
-    // 'vue/max-attributes-per-line': 'off',
-    // 'vue/html-self-closing': ['error', {
-    //   'html': {
-    //     void: always
-    //   }
-    // }]
+    'semi': [2, 'never'],
+    'no-console': 'off',
+    'vue/max-attributes-per-line': 'off',
+    'vue/html-self-closing': ['error', {
+      'html': {
+        void: always
+      }
+    }]
   }
 }
