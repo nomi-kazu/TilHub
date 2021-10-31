eb5e8c0d8b63ccf044b4c10d9137668b11ec00db
nomi-kazu <nomikazyu@gmail.com>
2021-10-31 11:43:45 +0900

    feat: inputを追加

---
+++ front/components/organisms/textFields/PasswordTextField.vue
@@ -0,0 +1,26 @@
+<template>
+  <!-- validationの処理 -->
+  <v-text-filed v-model="valueModel" />
+</template>
+
+<script>
+export default {
+  props: {
+    value: {
+      type: String,
+      default: undefined
+    }
+  },
+
+  computed: {
+    valueModel: {
+      get () {
+        return this.value
+      },
+      set (newVal) {
+        return this.$emit('input', newVal)
+      }
+    }
+  }
+}
+</script>
