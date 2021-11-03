63dc53026319a65be88621ab2443ee4efff54e36
nomi-kazu <nomikazyu@gmail.com>
2021-11-03 09:34:14 +0900

    style: レイアウト修正

--- front/components/organisms/btnGroup/LoginSnsBtnGroup.vue
+++ front/components/organisms/btnGroup/LoginSnsBtnGroup.vue
@@ -1,7 +1,7 @@
 <template>
-  <div>
-    <TwitterBtn>Twitter</TwitterBtn>
-    <GoogleBtn>Google</GoogleBtn>
+  <div class="d-flex flex-column">
+    <TwitterBtn class="mb-4">Twitter</TwitterBtn>
+    <GoogleBtn class="mb-4">Google</GoogleBtn>
     <GithubBtn>Github</GithubBtn>
   </div>
 </template>
