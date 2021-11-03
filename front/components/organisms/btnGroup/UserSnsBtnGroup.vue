c01c4dd7aa9e73902aeae226a782542d113c2754
nomi-kazu <nomikazyu@gmail.com>
2021-11-03 09:52:31 +0900

    feat: UserSnsBtnGroupを作成

--- front/components/organisms/btnGroup/UserSnsBtnGroup.vue
+++ front/components/organisms/btnGroup/UserSnsBtnGroup.vue
@@ -1,10 +1,22 @@
 <template>
   <div>
-    <!-- snsを紐付けるボタンをまとめる -->
+    <TwitterBtn fab :v-if="twitterLinkIsExists" />
+    <GithubBtn fab :v-if="githubLinkIsExists" />
   </div>
 </template>
 
 <script>
 export default {
+  computed: {
+    twitterLinkIsExists () {
+      // Twitterリンクが存在したらtrueを返す
+      return false
+    },
+
+    githubLinkIsExists () {
+      // Githubリンクが存在したらtrueを返す
+      return false
+    }
+  }
 }
 </script>
