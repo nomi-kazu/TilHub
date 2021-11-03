2843c451ff8e99091afc0a5a26652ea264ed918d
nomi-kazu <nomikazyu@gmail.com>
2021-11-03 16:27:17 +0900

    feat: 編集ページのフォーム追加

--- front/components/organisms/form/EditProfileForm.vue
+++ front/components/organisms/form/EditProfileForm.vue
@@ -1,12 +1,55 @@
 <template>
   <v-form>
-    <PreviewImageFileInput />
+    <div>
+      <p>プロフィール画像</p>
+      <div>
+        <v-img  src="https://picsum.photos/200/300?random" class="mr-2" />
+        <PreviewImageFileInput />
+      </div>
+    </div>
 
-    <!-- ユーザー名や自己紹介-->
+    <div class="mb-8">
+      <p>ユーザ名</p>
+      <v-text-field v-model="userName" outlined dense />
+    </div>
+
+    <div class="mb-8"> 
+      <p>自己紹介</p>
+      <v-textarea v-model="selfIntroduction" outlined height="80" />
+    </div>
+
+    <div class="mb-8">
+      <p>出身</p>
+      <v-text-field v-model="from" outlined dense />
+    </div>
   </v-form>
 </template>
 
 <script>
 export default {
+  data: () => {
+    return {
+      userName: undefined,
+      selfIntroduction: undefined,
+      from: undefined
+    }
+  },
+
+  created() {
+    // フォームに初期値を入れる
+  }
 }
 </script>
+
+<style>
+.v-form {
+  width: 80%;
+  text-align: left;
+}
+.v-img {
+  width: 45px;
+  height: 45px;
+  object-fit: cover;
+  border-radius: 50%;
+}
+</style>
