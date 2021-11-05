2516dcba514e7f4ebc042b218da86a4ca63a2a67
nomi-kazu <nomikazyu@gmail.com>
2021-11-05 21:39:00 +0900

    feat: pathの処理に関する関数を追加、テストを追加

--- front/jest.config.js
+++ front/jest.config.js
@@ -4,7 +4,8 @@ module.exports = {
   collectCoverageFrom: [
     '<rootDir>/components/**/*.vue',
     '<rootDir>/pages/**/*.vue',
-    '<rootDir>/store/**/*.js'
+    '<rootDir>/store/**/*.js',
+    '<rootDir>/utils/**/*.js'
   ],
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/$1',
