a5c7ae448a941ca3329bb36b0cd79bedcdeb6d50
nomi-kazu <nomikazyu@gmail.com>
2021-11-06 20:13:17 +0900

    feat: ファイルフォルダーリストの基礎を作成

--- front/components/templates/UsernameIndexTemplate.vue
+++ front/components/templates/UsernameIndexTemplate.vue
@@ -1,11 +1,11 @@
 <template>
-  <TwoColumnContainer left-cols="4" right-cols="6">
+  <TwoColumnContainer :left-cols="4" :right-cols="6">
     <template #left>
-      <UserIntroCard :user-info="userInfo" />
+      <UserIntroCard :user-info="userInfo" class="mt-10" />
     </template>
 
     <template #right>
-      <h2>あなたのリポジトリ</h2>
+      <h2 class="text-center">あなたのリポジトリ</h2>
 
       <FolderBreadcrumbs />
 
