a82f3301e79ab60196fb209c98e02b0570d8139f
nomi-kazu <nomikazyu@gmail.com>
2021-11-06 22:03:27 +0900

    feat: mockを作成

--- front/components/organisms/list/FileFolderListWithAction.vue
+++ front/components/organisms/list/FileFolderListWithAction.vue
@@ -3,25 +3,52 @@
     <!-- ファイルやフォルダーのリスト -->
     <v-list>
       <v-list-item-group>
-        <v-list-item>
+        <v-list-item v-for="(item, i) in list" :key="i">
           <v-list-item-icon>
-            <v-icon>mdi-folder</v-icon>
+            <v-icon v-if="item.isFolder">mdi-folder</v-icon>
+            <v-icon v-else>mdi-file</v-icon>
           </v-list-item-icon>
           <v-list-item-content>
             <v-list-item-title>
-              Docker入門
+              {{ item.title }}
             </v-list-item-title>
           </v-list-item-content>
         </v-list-item>
       </v-list-item-group>
     </v-list>
-    
+
     <!-- 編集や削除アクションを付けるList -->
   </div>
 </template>
 
 <script>
 export default {
-
+  props: {
+    // リストを受けとる
+    // list: {
+    //   type: Array,
+    //   default: undefined
+    // }
+  },
+  data: () => ({
+    list: [
+      {
+        title: "Docker",
+        isFolder: true
+      },
+      {
+        title: "CSS",
+        isFolder: true
+      },
+      {
+        title: "HTML",
+        isFolder: true
+      },
+      {
+        title: "Design.md",
+        isFolder: false
+      },
+    ]
+  })
 }
 </script>
