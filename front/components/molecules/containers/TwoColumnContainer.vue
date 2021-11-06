0dd0c8f702a0a4e6a4454d347e50a65252059975
nomi-kazu <nomikazyu@gmail.com>
2021-11-06 09:19:59 +0900

    feat: containerにpropsを追加

--- front/components/molecules/containers/TwoColumnContainer.vue
+++ front/components/molecules/containers/TwoColumnContainer.vue
@@ -1,11 +1,11 @@
 <template>
-  <BaseContainer>
+  <BaseContainer :fluid="fluid" :no-gutters="noGutters">
     <v-row :justify="justify">
-      <v-col :cols="leftCols">
+      <v-col :cols="leftCols" :sm="leftSm" :md="leftMd" :lg="leftLg" :xl="leftXl">
         <slot name="left" />
       </v-col>
 
-      <v-col :cols="rightCols">
+      <v-col :cols="rightCols" :sm="rightSm" :md="rightMd" :lg="rightLg" :xl="rightXl">
         <slot name="right" />
       </v-col>
     </v-row>
@@ -15,19 +15,69 @@
 <script>
 export default {
   props: {
+    fluid: {
+      type: Boolean,
+      default: false
+    },
+
+    noGutters: {
+      type: Boolean,
+      default: false
+    },
+
+    justify: {
+      type: String,
+      default: 'center'
+    },
+
     leftCols: {
       type: Number,
       default: 8
+    },
+
+    leftSm: {
+      type: [Boolean, String, Number],
+      default: false
+    },
+
+    leftMd: {
+      type: [Boolean, String, Number],
+      default: 12
+    },
+
+    leftLg: {
+      type: [Boolean, String, Number],
+      default: false
+    },
+
+    leftXl: {
+      type: [Boolean, String, Number],
+      default: false
     },
 
     rightCols: {
       type: Number,
       default: 4
     },
+  
+    rightSm: {
+      type: [Boolean, String, Number],
+      default: false
+    },
 
-    justify: {
-      type: String,
-      default: 'center'
+    rightMd: {
+      type: [Boolean, String, Number],
+      default: 12
+    },
+    
+    rightLg: {
+      type: [Boolean, String, Number],
+      default: false
+    },
+    
+    rightXl: {
+      type: [Boolean, String, Number],
+      default: false
     }
   }
 }
