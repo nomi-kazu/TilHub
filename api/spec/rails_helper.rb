60bbd6eeb569ba3f2408f754c40938ed35ffb40f
nomi-kazu <nomikazyu@gmail.com>
2021-11-08 18:52:43 +0900

    feat: テスト用の設定

--- api/spec/rails_helper.rb
+++ api/spec/rails_helper.rb
@@ -65,3 +65,7 @@ RSpec.configure do |config|
   # config.filter_gems_from_backtrace("gem name")
   config.include FactoryBot::Syntax::Methods
 end
+
+FactoryBot::SyntaxRunner.class_eval do
+  include ActionDispatch::TestProcess
+end
